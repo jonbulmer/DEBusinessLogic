45a8533b885e9e43443c9a8959b0f1c1673d0c65
Jon Bulmer <jon.bulmer@gmail.com>
2018-03-21 08:51:59 +0000

    finish of the value setting file

--- DE.IdentityServer.Settings/ViewModel/ValuesSetting.cs
+++ DE.IdentityServer.Settings/ViewModel/ValuesSetting.cs
@@ -15,5 +15,13 @@ namespace DE.IdentityServer.Settings.ViewModel
         public string post_logout_redirect_uri { get; set; }
         public bool start_checksession { get; set; }
         public bool silent_renew { get; set; }
+        public string startup_routine { get; set; }
+        public string forbidden_routine { get; set; }
+        public string unauthorized_routine { get; set; }
+        public bool log_console_warning_activity { get; set; }
+        public bool log_console_debug_activity { get; set; }
+        public string max_id_tocken_iat_offset_allowed_in_seconds { get; set; }
+        public string apiServer { get; set; }
+        public string apiFileServer { get; set; }
     }
 }
