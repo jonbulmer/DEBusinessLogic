36a4f33e0e75d318f69e3e91b6522dc88866cfb2
Jon Bulmer <jon.bulmer@gmail.com>
2018-03-22 07:58:21 +0000

    Controler set up

--- DE.IdentityServer.Settings/Controllers/ValuesController.cs
+++ DE.IdentityServer.Settings/Controllers/ValuesController.cs
@@ -1,44 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using DE.IdentityServer.Settings.ViewModel;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace DE.IdentityServer.Settings.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
-        // GET api/values
+        private readonly ValuesSetting _valuesSetting;
+
+        public ValuesController(IOptions<ValuesSetting> valuesSetting)
+        {
+            _valuesSetting = valuesSetting.Value;
+        }
+
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IActionResult Get()
         {
-            return new string[] { "value1", "value2" };
+            return Ok(_valuesSetting);
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
--- DE.IdentityServer.Settings/Startup.cs
+++ DE.IdentityServer.Settings/Startup.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using DE.IdentityServer.Settings.ViewModel;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
@@ -13,17 +15,30 @@ namespace DE.IdentityServer.Settings
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IHostingEnvironment env)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder().SetBasePath(env.ContentRootPath);
+            Configuration = builder.Build();
         }
 
-        public IConfiguration Configuration { get; }
+        public IConfigurationRoot Configuration { get; set; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<ValuesSetting>(Configuration.GetSection("ValuesSettings"));
             services.AddMvc();
+            services.AddCors(options =>
+            {
+                options.AddPolicy("",
+                    builder =>
+                    {
+                        builder
+                             .AllowAnyOrigin()
+                             .AllowAnyHeader()
+                             .AllowAnyMethod();
+                    });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +50,18 @@ namespace DE.IdentityServer.Settings
             }
 
             app.UseMvc();
+        }
+
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
         }
     }
 }
