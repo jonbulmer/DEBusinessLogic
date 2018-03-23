3d214d89d9229f05e3c3271239bf9f887b14ea1a
Jon Bulmer <jon.bulmer@gmail.com>
2018-03-23 08:09:46 +0000

    Finishing off setup

--- DE.IdentityServer.Settings/Startup.cs
+++ DE.IdentityServer.Settings/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using DE.IdentityServer.Settings.ViewModel;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Http;
 
 namespace DE.IdentityServer.Settings
 {
@@ -17,13 +18,16 @@ namespace DE.IdentityServer.Settings
     {
         public Startup(IHostingEnvironment env)
         {
-            var builder = new ConfigurationBuilder().SetBasePath(env.ContentRootPath);
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json",optional: true,reloadOnChange: true)
+                .AddJsonFile($"appsetting.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
             Configuration = builder.Build();
         }
 
         public IConfigurationRoot Configuration { get; set; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<ValuesSetting>(Configuration.GetSection("ValuesSettings"));
@@ -41,15 +45,51 @@ namespace DE.IdentityServer.Settings
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
 
-            app.UseMvc();
+            app.UseCors("AllowAllOrigins");
+
+            var angularRoutes = new[] {
+                "/home",
+                "/forbidden",
+                "/authorized",
+                "/authorize",
+                "/unauthorized",
+                "/dataeventrecords",
+                "/dataeventrecords/list",
+                "/dataeventrecords/create",
+                "/dataeventrecord/edit",
+                "/logoff",
+                "/securefiles",
+           };
+
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path.HasValue && null != angularRoutes.FirstOrDefault(
+                    (ar) => context.Request.Path.Value.StartsWith(ar,StringComparison.InvariantCultureIgnoreCase)))
+                {
+                    context.Request.Path = new PathString("/");
+                }
+                await next();
+            });
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+            routes.MapRoute(
+                name: "default",
+                template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("This is server routing,  not angular routing");
+            });
         }
 
         public static void Main(string[] args)
