8356c986c2b0ccfe748b9ee586ee51b0cdbcfb96
Jon Bulmer <jon.bulmer@gmail.com>
2018-04-03 01:59:42 +0100

    start on resource file sever

--- ResourceFileServer/Startup.cs
+++ ResourceFileServer/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -30,5 +31,18 @@ namespace ResourceFileServer
                 await context.Response.WriteAsync("Hello World!");
             });
         }
+
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+
     }
 }
