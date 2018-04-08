e501ddda4504ed26f1712883efc7f46de8b3e7de
Jon Bulmer <jon.bulmer@gmail.com>
2018-04-08 21:36:35 +0100

    Continue with set up

--- ResourceFileServer/Startup.cs
+++ ResourceFileServer/Startup.cs
@@ -28,11 +28,58 @@ namespace ResourceFileServer
         }
 
         public IConfigurationRoot Configuration { get; set; }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            //Add Cors support to the service
+            services.AddCors();
+
+            var policy = new Microsoft.AspNetCore.Cors.Infrastructure.CorsPolicy();
+
+            policy.Headers.Add("*");
+            policy.Methods.Add("*");
+            policy.Origins.Add("*");
+            policy.SupportsCredentials = true;
+
+            services.AddCors(x => x.AddPolicy("corsGlobalPolicy", policy));
+
+            var securedFilesPolicy = new AuthorizationPolicyBuilder()
+                .RequireAuthenticatedUser()
+                .RequireClaim("scope", "securedFiles")
+                .Build();
+
+            services.AddAuthentication(IdentityServerAuthenticationDefaults.AuthenticationScheme)
+               .AddIdentityServerAuthentication(options =>
+               {
+                   options.Authority = "https://localhost:44318/";
+                   options.ApiName = "securedFiles";
+                   options.ApiSecret = "securedFilesSecret";
+               });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("securedFilesUser", policyUser =>
+                {
+                    policyUser.RequireClaim("role", "securedFiles.user");
+                });
+                options.AddPolicy("securedFiles", policyUser =>
+                {
+                    policyUser.RequireClaim("scope", "securedFiles");
+                });
+            });
+
+            services.AddMvc(options =>
+            {
+                options.Filters.Add(new AuthorizeFilter(securedFilesPolicy));
+            });
+
+            services.AddMvc();
+
+            services.AddTransient<ISecuredFileProvider, SecuredFileProvider>();
+            services.AddSingleton<UseOnceAccessIdService>();
+
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
