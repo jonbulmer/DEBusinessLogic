03d1b6858bd9c0115c84a49ad3d3852bdd274d64
Jon Bulmer <jon.bulmer@gmail.com>
2018-04-10 07:36:12 +0100

    ISecureFileProvider

---
+++ ResourceFileServer/Providers/ISecureFileProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResourceFileServer.Providers
+{
+    public interface ISecureFileProvider
+    {
+        bool FileIdExists(string id);
+
+        bool HasUsedClaimToAccessFile(string fileId, bool isSecureFilesAdmin);
+    }
+}
