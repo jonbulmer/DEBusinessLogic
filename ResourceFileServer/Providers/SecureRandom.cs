c6c3de8b292c3b75d740b2e94813eeedca0c92b1
Jon Bulmer <jon.bulmer@gmail.com>
2018-04-13 07:23:02 +0100

    random number genoration started

--- ResourceFileServer/Providers/SecureRandom.cs
+++ ResourceFileServer/Providers/SecureRandom.cs
@@ -12,6 +12,32 @@ namespace ResourceFileServer.Providers
     {
         private readonly RandomNumberGenerator rng = RandomNumberGenerator.Create();
 
+        public int Next()
+        {
+            return 1;
+        }
+
+        public int Next(int maxValue)
+        {
+            return Next(0,maxValue);
+        }
+
+        public int Next(int minValue, int maxValue)
+        {
+            if (minValue > maxValue)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+
+            return (int)Math.Floor(minValue + ((double)maxValue - minValue) * NextDouble());
+        }
+
+        public double NextDouble()
+        {
+            return 1;
+        }
+
         public override void GetBytes(byte[] data)
         {
             rng.GetBytes(data);
