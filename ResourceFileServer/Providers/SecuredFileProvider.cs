baab8ac631d834e5efaf71cfbd876b6bd5ca236d
Jon Bulmer <jon.bulmer@gmail.com>
2018-04-18 06:32:32 +0100

    Work on the secured file provider

--- ResourceFileServer/Providers/SecuredFileProvider.cs
+++ ResourceFileServer/Providers/SecuredFileProvider.cs
@@ -5,7 +5,36 @@ using System.Threading.Tasks;
 
 namespace ResourceFileServer.Providers
 {
-    public class SecuredFileProvider
+    public class SecuredFileProvider : ISecuredFileProvider
     {
+        public string AddFileIdForUseOnceAccessId(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool FileIdExists(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetFileIdForUseOnceAccessId(string useOnceAccessId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> GetFileIdForUser(bool isSecuredFileAdmin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool HasUsedClaimToAccessFile(string fileId, bool isSecureFilesAdmin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool HasUserClaimToAccessFile(string fileId, bool isSecuredFilesAdmin)
+        {
+            return false;
+        }
     }
 }
