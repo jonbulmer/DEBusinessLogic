b41908619e41211a1069011755611e8975017756
Jon Bulmer <jon.bulmer@gmail.com>
2018-04-20 17:42:17 +0100

    added the constructor to Use once access id service

---
+++ ResourceFileServer/Providers/UseOnceAccessIdService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResourceFileServer.Providers
+{
+    public class UseOnceAccessIdService
+    {
+        private double _timeToLive = 30.0;
+        private static object lockObject = new Object();
+
+        private List<UseOnceAccessIdService> _useOnceAccessIds = new List<UseOnceAccessId>();  
+    }
+}
