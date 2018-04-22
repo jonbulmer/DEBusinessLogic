b82a50233e5d3b2c6cf7f2a112f729846d727312
Jon Bulmer <jon.bulmer@gmail.com>
2018-04-22 12:30:34 +0100

    More work with use once accessid service

---
+++ ResourceFileServer/Providers/UseOnceAccessId.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResourceFileServer.Providers
+{
+    internal class UseOnceAccessId
+    {
+        public UseOnceAccessId(string fileId)
+        {
+            Created = DateTime.UtcNow;
+            AccessId = CreatedAccessId();
+            FileId = fileId;
+        }
+
+        public DateTime Created { get; }
+
+        public string AccessId { get; }
+
+        public string FileId { get; }
+
+        public string CreatedAccessId ()
+        {
+            SecureRandom secureRandom = new SecureRandom();
+            return secureRandom.Next() + Guid.NewGuid().ToString();
+        }
+    }
+}
--- ResourceFileServer/Providers/UseOnceAccessIdService.cs
+++ ResourceFileServer/Providers/UseOnceAccessIdService.cs
@@ -8,8 +8,21 @@ namespace ResourceFileServer.Providers
     public class UseOnceAccessIdService
     {
         private double _timeToLive = 30.0;
-        private static object lockObject = new Object();
+        private static object lockObject = new object();
 
-        private List<UseOnceAccessIdService> _useOnceAccessIds = new List<UseOnceAccessId>();  
+        private List<UseOnceAccessId> _useOnceAccessIds = new List<UseOnceAccessId>();
+
+        public string GetFileIdForUseOnceAccesId(string useOnceAccessId)
+        {
+            var fileId = string.Empty;
+
+            lock (lockObject) 
+            {
+                // Max 30 seconds to start download after requesting one time tocken.
+                _useOnceAccessIds.RemoveAll(t => t.Created < DateTime.UtcNow.AddSeconds(-_timeToLive));
+            }
+
+            return fileId;
+        }
     }
 }
