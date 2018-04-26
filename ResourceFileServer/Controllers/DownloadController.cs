caa747b995eaa3b0e1f6bc6acf28e9f03be4e261
Jon Bulmer <jon.bulmer@gmail.com>
2018-04-26 07:22:50 +0100

    Started on the down load  controller

---
+++ ResourceFileServer/Controllers/DownloadController.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using ResourceFileServer.Providers;
+using Microsoft.AspNetCore.Hosting;
+
+namespace ResourceFileServer.Controllers
+{
+    [Route("api/[controller]")]
+    public class DownloadController : Controller
+    {
+        public DownloadController()
+        {
+
+        }
+
+        [AllowAnonymous]
+        [HttpGet("{accessId}/{id}")]
+        public IActionResult Get()
+        {
+
+        }
+
+
+        public IActionResult GenorateOneTimeAccessToken()
+        {
+
+        }
+
+
+    }
+}
