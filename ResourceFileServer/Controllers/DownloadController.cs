cf9e050775862aa35346e564b31670427bf6154c
Jon Bulmer <jon.bulmer@gmail.com>
2018-04-27 07:21:17 +0100

    work on the download controller

--- ResourceFileServer/Controllers/DownloadController.cs
+++ ResourceFileServer/Controllers/DownloadController.cs
@@ -10,20 +10,29 @@ namespace ResourceFileServer.Controllers
     [Route("api/[controller]")]
     public class DownloadController : Controller
     {
-        public DownloadController()
+        private readonly IHostingEnvironment _appEnvironment;
+        private readonly ISecuredFileProvider _securedFilePathProvider;
+        public DownloadController(ISecuredFileProvider securedFileProvider, IHostingEnvironment appEnvironment)
         {
-
+            _securedFilePathProvider = securedFileProvider;
+            _appEnvironment = appEnvironment;
         }
 
         [AllowAnonymous]
         [HttpGet("{accessId}/{id}")]
-        public IActionResult Get()
+        public IActionResult Get(string accessId, string id)
         {
-
+            var filePath = _securedFilePathProvider.GetFileIdForUseOnceAccessId(accessId);
+            if(!string.IsNullOrEmpty(filePath))
+            {
+                var fileContents = System.IO.File.ReadAllBytes(filePath);
+                return new FileContentResult(fileContents, 'application/octet-stream');
+            }
+            return new StatusCodeResult(401);
         }
 
 
-        public IActionResult GenorateOneTimeAccessToken()
+        public IActionResult GenorateOneTimeAccessToken(string  id)
         {
 
         }
