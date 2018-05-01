a59c65ae1b3204aef950593eed81b3a313fae3b0
Jon Bulmer <jon.bulmer@gmail.com>
2018-05-01 07:25:15 +0100

    Start work on the File Exploer Controller

---
+++ ResourceFileServer/Controllers/FileExploerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using ResourceFileServer.Providers;
+
+namespace ResourceFileServer.Controllers
+{
+    public class FileExploerController : Controller
+    {
+        public FileExploerController()
+        {
+
+        }
+
+        public IActionResult Get()
+        {
+            // TODO MUST FIX. rc4 update does not work. claims are missing
+            var adminClaim
+            var files = 
+            return Ok(files); 
+        }
+    }
+}
