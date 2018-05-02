c2f5cd1eed7625e6ac4b0b5f09ac802f4d53d988
Jon Bulmer <jon.bulmer@gmail.com>
2018-05-02 07:19:59 +0100

    complete thexplor e setup for File Explorer controller

--- ResourceFileServer/Controllers/FileExploerController.cs
+++ ResourceFileServer/Controllers/FileExploerController.cs
@@ -5,18 +5,25 @@ using ResourceFileServer.Providers;
 
 namespace ResourceFileServer.Controllers
 {
+    [Authorize("securedFiles")]
+    [Route("api/[controler]")]
     public class FileExploerController : Controller
     {
-        public FileExploerController()
-        {
+        private readonly ISecuredFileProvider _securedFileProvider;
 
+
+        public FileExploerController(ISecuredFileProvider securedFileProvider)
+        {
+            _securedFileProvider = securedFileProvider;
         }
 
+        [HttpGet]
         public IActionResult Get()
         {
             // TODO MUST FIX. rc4 update does not work. claims are missing
-            var adminClaim
-            var files = 
+            var adminClaim = User.Claims.FirstOrDefault(x => x.Type == "role" && x.Value == "secured.admin");
+            var files = _securedFileProvider.GetFileIdForUser(adminClaim != null);
+
             return Ok(files); 
         }
     }
