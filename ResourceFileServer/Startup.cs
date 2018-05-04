0e0beaee5486b99c9d4fb7d8aa4578e8c69eda03
Jon Bulmer <jon.bulmer@gmail.com>
2018-05-04 07:21:46 +0100

    the changes needed to finish the resource file server set up

--- ResourceFileServer/Startup.cs
+++ ResourceFileServer/Startup.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Authentication;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
