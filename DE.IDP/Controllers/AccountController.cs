8e7210792b8bc64770e5f596b8a3f9110f6da748
Jon Bulmer <jon.bulmer@gmail.com>
2018-05-13 20:57:01 +0100

    Building the login logic

---
+++ DE.IDP/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using DE.IDP.Models;
+
+namespace DE.IDP.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        public AccountController(
+            UserManger<Application> userManger
+            )
+        {
+
+        }
+    }
+}
