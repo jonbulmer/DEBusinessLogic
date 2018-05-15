f6bde38738cc770c60db2f8d5c5b318255f46026
Jon Bulmer <jon.bulmer@gmail.com>
2018-05-15 07:20:42 +0100

    continued set up

--- DE.IDP/Controllers/AccountController.cs
+++ DE.IDP/Controllers/AccountController.cs
@@ -3,16 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using DE.IDP.Models;
+using DE.IDP.Models.AccountViewModels;
+using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Extensions;
+using IdentityServer4.Services;
+using IdentityServer4.Stores;
+using IdentityServer4.Models;
+
 
 namespace DE.IDP.Controllers
 {
     [Authorize]
     public class AccountController : Controller
     {
+        private readonly string a;
+        private readonly string b;
+        private readonly string c;
+        private readonly string d;
+        private readonly string e;
+        private readonly string f;
+        private readonly string g;
+        private readonly string h;
+
         public AccountController(
-            UserManger<Application> userManger
+            UserManger<ApplicationUser> userManger
             )
         {
 
